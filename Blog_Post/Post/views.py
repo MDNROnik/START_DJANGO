9baeed95142ea08defa76f5b4f1a248469d1153c
Md. Nimur Rahman Onik <nimurrahnananik@gmail.com>
2024-05-13 10:25:48 +0600

    Django Blog

---
+++ Blog_Post/Post/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Posts
+# Create your views here.
+def index(request):
+    posts = Posts.objects.all()
+    return render(
+        request,
+        'index.html',
+        {'posts': posts}
+    )
+    
+def post(request, pk):
+    print(pk)
+    post = Posts.objects.get(id=pk)
+    return render(
+        request,
+        'post.html',
+        {'posts': post}
+    )
