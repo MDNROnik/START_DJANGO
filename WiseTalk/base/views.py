3c466105577614d75502f8ee62a19ec21cd8f074
Md. Nimur Rahman Onik <nimurrahnananik@gmail.com>
2025-10-31 20:23:22 +0100

    Implement Room model and views, update URL routing, and create templates for home and room pages

--- WiseTalk/base/views.py
+++ WiseTalk/base/views.py
@@ -2,15 +2,25 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
+
+# room_names = [
+#     {'id': 1, 'name': 'Python Programming'},
+#     {'id': 2, 'name': 'Django Framework'},
+# ]
 
 
 def home(req):
     print("Home page accessed ->", req)
-    return render(req, 'home.html')
+    room_names = Room.objects.all() 
+    context = {'rooms': room_names}
+    return render(req, 'base/home.html', context)
 
 
-def room(req):
+def room(req, pk):
     print("Room page accessed ->", req)
-    return render(req, 'room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room_name': room}
+    return render(req, 'base/room.html', context)
